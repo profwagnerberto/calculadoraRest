105c7fe9b24181c168305563e2ed00f545fddb0f
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-07-10 15:25:23 -0300

    Pacote de controladores e Controlador Principal para o endpoint /calculadora, criados.

---
+++ src/main/java/br/com/empresa/calculadoraRest/controladores/ControladorPrincipal.java
@@ -0,0 +1,9 @@
+package br.com.empresa.calculadoraRest.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculadora")
+public class ControladorPrincipal {
+}
